95d01b46b49bb08ed6440dd6694ffa64d177197e
Angela Stephens <angelastephens317@gmail.com>
2020-06-03 01:58:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 end
 
 def assign_rooms(attendees)
-  atttendess.each_with_index.map do |attendee,index|
+  atttendee.each_with_index.map do |attendee,index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 end
