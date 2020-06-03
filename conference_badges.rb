6536d124210ecdd529932c15bf1d7565908e4123
Angela Stephens <angelastephens317@gmail.com>
2020-06-03 01:14:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  binding.pry 
+  array="badge messages"
+  loop do
+ return "Hello, my name is #{name}."
+ break
+end
+end
+
